f4d284dcc70081c142d90c90fd97343b99897f56
Ewald Horn <ewaldhorn@gmail.com>
2020-09-17 16:36:02 +0200

    Just trying stuff

---
+++ app/src/main/java/za/co/nofuss/funnamathics/activity/AboutActivity.kt
@@ -0,0 +1,12 @@
+package za.co.nofuss.funnamathics.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import za.co.nofuss.funnamathics.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
--- app/src/main/java/za/co/nofuss/funnamathics/activity/GameActivity.kt
+++ app/src/main/java/za/co/nofuss/funnamathics/activity/GameActivity.kt
@@ -1,11 +1,12 @@
-package za.co.nofuss.funnamathics
+package za.co.nofuss.funnamathics.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import za.co.nofuss.funnamathics.R
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
     }
 }
